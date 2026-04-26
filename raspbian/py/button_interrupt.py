e3a4d9ad78371ee88d84ca5e119653a5321309af
npmast <126345447+npmast@users.noreply.github.com>
2026-04-26 22:06:22 +0900

    Create button_interrupt.py

---
+++ raspbian/py/button_interrupt.py
@@ -0,0 +1,18 @@
+from gpiozero import LED, Button
+from signal import pause
+
+led = LED(14)
+button = Button(18)
+
+def button_pressed():
+    led.on()
+    print("BUTTON PRESSED")
+
+def button_released():
+    led.off()
+    print("BUTTON RELEASED")
+
+button.when_pressed = button_pressed
+button.when_released = button_released
+
+pause()
