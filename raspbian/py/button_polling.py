cac536f9a1f9850b543e86dea0992bd5ebeff5e2
npmast <126345447+npmast@users.noreply.github.com>
2026-04-27 19:08:57 +0900

    Update button_polling.py

--- raspbian/py/button_polling.py
+++ raspbian/py/button_polling.py
@@ -4,12 +4,16 @@ from time import sleep
 led = LED(14)          # GPIO14
 button = Button(18)    # GPIO18, 기본 pull_up=True
 
-while True:
-    if button.is_pressed:
-        led.on()
-        print("BUTTON ON")
-    else:
-        led.off()
-        print("BUTTON OFF")
+try:
+    while True:
+        if button.is_pressed:
+            led.on()
+            print("BUTTON ON")
+        else:
+            led.off()
+            print("BUTTON OFF")
 
-    sleep(0.1)
+        sleep(0.1)
+except KeyboardInterrupt:
+    led.close()
+    print("\nprogram exit")
