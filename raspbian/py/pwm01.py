5a85fff9b884a83ac8935485217be1952ae27eb4
npmast <126345447+npmast@users.noreply.github.com>
2026-04-28 18:56:01 +0900

    Create pwm01.py

---
+++ raspbian/py/pwm01.py
@@ -0,0 +1,16 @@
+from gpiozero import PWMLED
+from time import sleep
+
+led = PWMLED(17)
+
+try:
+    while True:
+        led.value = 0  # off
+        sleep(1)
+        led.value = 0.5  # half brightness
+        sleep(1)
+        led.value = 1  # full brightness
+        sleep(1)
+except KeyboardInterrupt:
+    led.close()
+    print("\nprogram exit")
