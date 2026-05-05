3c0238579f392ae696f0e535a56a50386c7460c3
npmast <126345447+npmast@users.noreply.github.com>
2026-05-05 16:20:44 +0900

    Create appTest02.py

---
+++ raspbian/Flask/appTest02.py
@@ -0,0 +1,25 @@
+# route 데코레이터 입력
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Flask Server Test"
+
+#변수규칙
+@app.route('/user/<username>')                    # URL에서 <username>을 매개변수로 받는다
+def user_profile(username):
+	return "User %s" % username
+
+@app.route('/pw/<int:pw_num>')
+def show_pw(pw_num):
+	return 'pw %d' % pw_num
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True)
+
+'''
+가변적인 데이터를 뷰 함수로 전달받는다.
+'''
