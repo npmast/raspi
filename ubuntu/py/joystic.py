84ccbbde422b3731d66cc42b75848cd7b73526cf
npmast <126345447+npmast@users.noreply.github.com>
2026-05-05 15:27:29 +0900

    Create joystic.py

---
+++ ubuntu/py/joystic.py
@@ -0,0 +1,26 @@
+#### joystic
+import smbus
+import time
+from gpiozero import Button
+
+bus = smbus.SMBus(1)
+ADDR = 0x48  # PCF8591 기본 주소
+
+button = Button(17)
+
+def read_adc(channel):
+    bus.write_byte(ADDR, 0x40 | channel)
+    bus.read_byte(ADDR)   # dummy read
+    return bus.read_byte(ADDR)
+
+try:
+    while True:
+        x = read_adc(0)
+        y = read_adc(1)
+        sw = button.is_pressed
+
+        print(f"X: {x}, Y: {y}, SW: {sw}")
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    print("종료")
